7a13b47113c24807c1d64adfdf65db9697fec3d2
Paichien <b8350580@gmail.com>
2023-06-29 20:30:08 +0800

    basic routes set up

--- Develop/controllers/api/userRoutes.js
+++ Develop/controllers/api/userRoutes.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { Users } = require('../../models');
 
 router.post('/', async (req, res) => {
   try {
-    const userData = await User.create(req.body);
+    const userData = await Users.create({...req.body});
 
     req.session.save(() => {
       req.session.user_id = userData.id;
@@ -18,7 +18,7 @@ router.post('/', async (req, res) => {
 
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
+    const userData = await Users.findOne({ where: { email: req.body.email } });
 
     if (!userData) {
       res
@@ -48,7 +48,7 @@ router.post('/login', async (req, res) => {
   }
 });
 
-router.get('/logout', (req, res) => {
+router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
       res.status(204).end();
