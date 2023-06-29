79e1c71195ae9123e5409748a406fad9841ba674
Brenden <53604923+Brenden-uni@users.noreply.github.com>
2023-06-29 21:19:08 +0800

    seeds

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,26 +1,18 @@
 const Users = require('./Users');
 const Skills = require('./Skills');
-const Skill_User = require('./skill_user');
-
-Users.hasMany(Skill_User, {
-  foreignKey: 'user_id',
-});
-
-Skill_User.belongsTo(Users, {
-  foreignKey: 'user_id'
-});
+const Skill_User = require('./Skill_User');
 
 Users.belongsToMany(Skills, {
   through:{
     model:Skill_User, as: 'skills',
-    foreignKey: 'user_id'
+    unique: false,
   }
 });
 
 Skills.belongsToMany(Users, {
   through:{
     model:Skill_User, as: 'users',
-    foreignKey: 'skill_id'
+    unique: false,
   }
 })
 module.exports = { Users, Skills, Skill_User };
--- Develop/seeds/seed.js
+++ Develop/seeds/seed.js
@@ -1,8 +1,9 @@
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const { User, Skill } = require('../models');
 
 const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const skillData = require('./skillData.json');
+const skill_userData = require('./skill_userData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,9 +13,16 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
+  for (const skill of skillData) {
+    await Skill.create({
+      ...skill,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  for (const skill_user of skill_userData) {
+    await Skill_user.create({
+      ...skill_user,
       user_id: users[Math.floor(Math.random() * users.length)].id,
     });
   }
